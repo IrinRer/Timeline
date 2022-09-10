f2d1e5dd3fd64d4c3752cde095afcfd8cd06d23f
Irin <iraantonova1998@list.ru>
2022-09-10 10:39:10 +0300

    change type

--- src/store/historyDate/types.ts
+++ src/store/historyDate/types.ts
@@ -15,24 +15,32 @@ export interface IHitoryDateSlice {
 
 export interface IHistoryDateItem {
   first: {
+    temporary_period_start: number;
+    temporary_period_end: number;
     science: Array<IHistoryDatePeriod>;
     movie: Array<IHistoryDatePeriod>;
     literature: Array<IHistoryDatePeriod>;
     music: Array<IHistoryDatePeriod>;
   };
   second: {
+    temporary_period_start: number;
+    temporary_period_end: number;
     science: Array<IHistoryDatePeriod>;
     movie: Array<IHistoryDatePeriod>;
     literature: Array<IHistoryDatePeriod>;
     music: Array<IHistoryDatePeriod>;
   };
   third: {
+    temporary_period_start: number;
+    temporary_period_end: number;
     science: Array<IHistoryDatePeriod>;
     movie: Array<IHistoryDatePeriod>;
     literature: Array<IHistoryDatePeriod>;
     music: Array<IHistoryDatePeriod>;
   };
   fourth: {
+    temporary_period_start: number;
+    temporary_period_end: number;
     science: Array<IHistoryDatePeriod>;
     movie: Array<IHistoryDatePeriod>;
     literature: Array<IHistoryDatePeriod>;
